71eaa614345e9155bf22939b8200f2561320bb05
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-21 21:42:31 +0600

    Learn About Backtick in Es6

---
+++ module-01/template.js
@@ -0,0 +1,35 @@
+
+// Used of Backtick
+
+const first = 'Jann';
+const last = 'pakhi';
+const greet = 'Potash Potash';
+
+const name = first + ' ' + last + ' ' + greet;
+console.log(name);
+
+
+const a = 10;
+const b = 20;
+
+const result = 'The Sum of : ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(result);
+
+const numbers = [45,48,66,98];
+const students = {name:'jhon Cena',age:5}
+
+// const math = `The Sum of : ${a} and ${b} is ${a+b}`;
+const math = `The Sum of : ${numbers[0]} and ${students.age} is ${numbers[0] + students.age}`;
+console.log(math)
+
+const email = 'Hi Joncena \n'+
+'cena cena Lage \n' +
+'Mara mari korba naki';
+console.log(email);
+
+
+const challenge = `Hi joncena
+cena naki ocena
+mar dimu prochur`;
+
+console.log(challenge);
