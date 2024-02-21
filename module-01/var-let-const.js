e1a2d54c2f264ad210a9eac54e46d871c4e1ac12
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-21 20:38:12 +0600

    About let const and var

--- module-01/var-let-const.js
+++ module-01/var-let-const.js
@@ -7,3 +7,12 @@ const money = 25;
 //money = 50;
 const rich = money + 25;
 console.log(rich);
+
+
+let count = 0;
+count = count + 10;
+console.log(count)
+
+const number = [23,4,23,56,22];
+// number = [4,5,7,7];
+console.log(number)
